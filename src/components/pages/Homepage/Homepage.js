f32c51c104fbf083098f43bd55955fb3ae29e0c2
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-08-18 12:33:29 +0530

    favicon set for title

--- src/components/pages/Homepage/Homepage.js
+++ src/components/pages/Homepage/Homepage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getAllTours } from "../../../actions/userActions";
 
