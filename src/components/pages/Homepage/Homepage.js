a5a7c6d85a4b41f245e247746f7a789b93670cd8
Ashutosh Maurya <ashutoshm9695@gmail.com>
2023-02-05 09:43:44 +0530

    loading

--- src/components/pages/Homepage/Homepage.js
+++ src/components/pages/Homepage/Homepage.js
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getAllTours } from "../../../actions/userActions";
@@ -6,12 +7,14 @@ import "../../../style.css";
 
 const Homepage = () => {
   const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const _getallTours = async () => {
     const tour = await getAllTours();
     if (tour) {
       let data = tour.data?.doc;
       console.log(tour);
+      setLoading(false);
       setTours(data);
     }
   };
@@ -23,74 +26,88 @@ const Homepage = () => {
   return (
     <div className="main">
       <div className="card-container">
-        {tours.map((item, index) => (
-          <div className="card" key={index}>
-            <div className="card__header">
-              <div className="card__picture">
-                <div className="card__picture-overlay">&nbsp;</div>
-                <img
-                  className="card__picture-img"
-                  src={item.imageCover}
-                  alt="Tour 1"
-                />
+        {!loading ? (
+          tours.map((item, index) => (
+            <div className="card" key={index}>
+              <div className="card__header">
+                <div className="card__picture">
+                  <div className="card__picture-overlay">&nbsp;</div>
+                  <img
+                    className="card__picture-img"
+                    src={item.imageCover}
+                    alt="Tour 1"
+                  />
+                </div>
+                <h3 className="heading-tertirary">
+                  <span>{item.name}</span>
+                </h3>
               </div>
-              <h3 className="heading-tertirary">
-                <span>{item.name}</span>
-              </h3>
+              <div className="card__details">
+                <h4 className="card__sub-heading">
+                  {item.difficulty} {item.duration}-day tour
+                </h4>
+                <p className="card__text">{item.summary}</p>
+                <div className="card__data">
+                  <svg className="card__icon">
+                    <use xlinkHref="img/icons.svg#icon-map-pin" />
+                  </svg>
+                  <span>{item.startLocation.description}</span>
+                </div>
+                <div className="card__data">
+                  <svg className="card__icon">
+                    <use xlinkHref="img/icons.svg#icon-calendar" />
+                  </svg>
+                  <span>{item.startDates[0]}</span>
+                </div>
+                <div className="card__data">
+                  <svg className="card__icon">
+                    <use xlinkHref="img/icons.svg#icon-flag" />
+                  </svg>
+                  <span>{item.locations.length} stops</span>
+                </div>
+                <div className="card__data">
+                  <svg className="card__icon">
+                    <use xlinkHref="img/icons.svg#icon-user" />
+                  </svg>
+                  <span>{item.maxGroupSize} people</span>
+                </div>
+              </div>
+              <div className="card__footer">
+                <p>
+                  <span className="card__footer-value">Rs.{item.price}</span>
+                  <span className="card__footer-text">per person</span>
+                </p>
+                <p className="card__ratings">
+                  <span className="card__footer-value">
+                    {item.ratingsAverage}
+                  </span>
+                  <span className="card__footer-text">
+                    rating ({item.ratingsQuantity})
+                  </span>
+                </p>
+                <Link
+                  className="btn btn--green btn--small"
+                  to="/tourDetails"
+                  state={item._id}
+                >
+                  Details
+                </Link>
+              </div>
             </div>
-            <div className="card__details">
-              <h4 className="card__sub-heading">
-                {item.difficulty} {item.duration}-day tour
-              </h4>
-              <p className="card__text">{item.summary}</p>
-              <div className="card__data">
-                <svg className="card__icon">
-                  <use xlinkHref="img/icons.svg#icon-map-pin" />
-                </svg>
-                <span>{item.startLocation.description}</span>
-              </div>
-              <div className="card__data">
-                <svg className="card__icon">
-                  <use xlinkHref="img/icons.svg#icon-calendar" />
-                </svg>
-                <span>{item.startDates[0]}</span>
-              </div>
-              <div className="card__data">
-                <svg className="card__icon">
-                  <use xlinkHref="img/icons.svg#icon-flag" />
-                </svg>
-                <span>{item.locations.length} stops</span>
-              </div>
-              <div className="card__data">
-                <svg className="card__icon">
-                  <use xlinkHref="img/icons.svg#icon-user" />
-                </svg>
-                <span>{item.maxGroupSize} people</span>
-              </div>
-            </div>
-            <div className="card__footer">
-              <p>
-                <span className="card__footer-value">Rs.{item.price}</span>
-                <span className="card__footer-text">per person</span>
-              </p>
-              <p className="card__ratings">
-                <span className="card__footer-value">
-                  {item.ratingsAverage}
-                </span>
-                <span className="card__footer-text">
-                  rating ({item.ratingsQuantity})
-                </span>
-              </p>
-              <Link
-                className="btn btn--green btn--small"
-                to="/tourDetails"
-                state={item._id}
-              >
-                Details
-              </Link>
-            </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <>
+            <Box></Box>
+            <Box
+              display="flex"
+              justifyContent="center"
+              style={{ textAlign: "center", fontSize: "64px" }}
+            >
+              {" "}
+              Loading...
+            </Box>
+          </>
+        )}
       </div>
     </div>
   );
